de04435ebf79438d1c117b6ebefab248c2d7ddcf
Ábner Oliveira <abner.silva@gmail.com>
2013-02-22 11:54:54 -0500

    resultado dojo

---
+++ js/jogodamemoria.js
@@ -0,0 +1,110 @@
+/***
+Dojo no DESDR =>  Javascript + jQuery + Jasmine
+
+Jasmine: http://pivotal.github.com/jasmine/
+jQuery: http://api.jquery.com/
+
+Jogo da Memória
+
+ - Especificação
+     - jogo da memória com 6 imagens
+     - tabuleiro começa vazio e a ordem das peças deve ser definida. 
+     - ao clicar na carta a mesma é virada e exibida ao usuário, e seu sequencial deve ser armazenadao
+     - no clique subsequente, se a imagem da carta for a mesma do elemento anterior, então as duas permanecem exibidas
+     - caso a imagem na segunda carta virada não coincida com a carta anterior, as duas são desviradas
+     - o jogo acaba quando todas as cartas estiverem viradas
+     - o usuário deve ser notificado de que o jogo finalizou
+     - ao encontrar um par, as cartas do par terão um efeito de realcce(highlight) como feedback para o usuário
+     
+Recomendações
+
+    - Prioridade para a parte lógica do jogo. Vamos implementar os requisitos através de Desenvolvimento Orientado a Testes, usando o framework jasmine
+    - Após a implementação da lógica faremos a finalização da interface com o usuário. E vamos seguir a mesma metodologia de Teste em primeiro lugar para essa etapa.
+    - Vamos trabalhar em pares, com piloto e co-piloto. A dupla deve verbalizar o trabalho executado.
+    - A audiência deve observar em silêncio e ajudar apenas quando solicitada pela dupla no comando. 
+***/
+
+var host_images = "./images/";
+
+var images = [
+    { id:'boy', filename: 'Character%20Boy.png' },
+    { id: 'bug', filename: 'Enemy%20Bug.png'},
+    { id: 'gem-blue', filename: 'Gem%20Blue.png'},
+    { id: 'gem-green', filename: 'Gem%20Green.png'},
+    { id: 'gem-orange', filename: 'Gem%20Orange.png'},
+    { id: 'rock', filename: 'Rock.png'}
+    ];
+
+
+var tabuleiro = [];
+
+//for para ilustrar como iterar no array
+for(index in images) {
+    console.log(images[index].id);
+    console.log(images[index].filename);
+}
+
+function obterUrlImagem(imagem) {
+    return host_images + imagem.filename;
+}
+
+jQuery(function($) {
+  //colocar aqui o código relacionado com a interface visual e iteração com o usuário
+});
+
+//FUNCOES UTILITARIAS
+/**
+ * Randomize array element order in-place.
+ * Using Fisher-Yates shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+Array.prototype.shuffle = function() {
+  var i = this.length, j, tempi, tempj;
+  if ( i == 0 ) return false;
+  while ( --i ) {
+     j       = Math.floor( Math.random() * ( i + 1 ) );
+     tempi   = this[i];
+     tempj   = this[j];
+     this[i] = tempj;
+     this[j] = tempi;
+  }
+  return this;
+}
+
+function montarDeck() {
+    var deck = [];
+    
+    for(i = 0; i < 6; i++) {
+        deck.push({indice_imagem: i, virado: false});
+        deck.push({indice_imagem: i, virado: false});
+    }
+    
+    return deck;
+}
+
+function virarCarta(deck, indexCarta) {
+    if (indexCarta==selecionada){
+        return;
+    }
+  deck[indexCarta].virado = true; 
+    if(selecionada == -1) {
+        selecionada = indexCarta;
+    } else {
+        var acertada = (deck[indexCarta].index_imagem == deck[selecionada].index_imagem);
+        deck[indexCarta].acertada = acertada;
+        deck[selecionada].acertada = acertada;
+        
+        selecionada = -1;
+    }
+}
+
+deck = montarDeck().shuffle();
---
+++ js/jogodamemoria.spec.js
@@ -0,0 +1,128 @@
+    /* TESTES */
+
+    describe('Array de imagens', function() {
+            it('deve conter  6 imagens', function() {
+                expect(images.length).toBe(6);
+            });
+        });
+    
+    describe('Função obterUrlImagem', function() {
+        it('deve retornar url com o endereço da imagem', function(){
+           expect(obterUrlImagem(images[0])).toBe("./images/Character%20Boy.png");
+        });
+    });
+    describe("Embaralhar array deve retornar um array", function() {
+        
+        var array = [1,2,3,4,5,6,7,8,9,10];
+        var array_embaralhado = array.shuffle();
+        
+        it("com os elementos numa ordem diferente da ordem do array fornecido", function() {
+            expect(array_embaralhado.join()).not.toBe("1,2,3,4,5,6,7,8,9,10");
+        });
+        it("com o mesmo tamanho", function() {
+            expect(array.length).toBe(array_embaralhado.length);
+        });
+        it("com os mesmos elementos", function() {
+            
+            expect(array_embaralhado).toContain(1);
+            expect(array_embaralhado).toContain(2);
+            expect(array_embaralhado).toContain(3);
+            expect(array_embaralhado).toContain(4)
+            expect(array_embaralhado).toContain(5);
+            expect(array_embaralhado).toContain(6);
+            expect(array_embaralhado).toContain(7);
+            expect(array_embaralhado).toContain(8);
+            expect(array_embaralhado).toContain(9);
+            expect(array_embaralhado).toContain(10);
+        });
+    });
+    
+    describe("Montagem do deck", function() {
+        it("deve conter 12 elementos", function() {
+            var deck = montarDeck();
+            expect(deck.length).toBe(12);
+        });
+    
+        it("deve conter 12 cartas válidas", function() {
+            var deck = montarDeck();
+            for(i=0; i < 12 ; i++){
+              expect(deck[i].indice_imagem).toBe(Math.floor(i/2));
+            }
+        });
+        
+        it("todas as acertadas nao serem true", function() {
+            var deck = montarDeck();
+            for(i=0; i < 12 ; i++){
+              expect(deck[i].acertade).not.toBe(true);
+            }
+        });
+    
+    });
+
+    describe("Virar carta", function() {
+        
+        var deck = null;
+        
+        beforeEach(function() {
+            deck = montarDeck();
+            selecionada = -1;
+        });
+        
+        it("altera a flag virado para true", function(){
+            
+            virarCarta(deck, 0);
+            expect(deck[0].virado).toBe(true);
+        });
+        
+        it ("altera somente a carta indicada", function() {
+            var posicao = Math.floor((Math.random()*12));
+            virarCarta(deck, posicao);
+            for(i=0; i < 12; i++){
+                expect(deck[i].virado).toBe(i == posicao);
+            }
+        });    
+        it("marcar carta como selecionada", function(){
+            virarCarta(deck, 0);
+            expect(selecionada).toBe(0);
+        });
+        it("virar segunda carta", function(){
+            virarCarta(deck, 0);
+            virarCarta(deck, 1);
+            expect(selecionada).toBe(-1);
+        });
+        
+        it("reinicia selecao", function(){
+            virarCarta(deck, 0);
+            virarCarta(deck, 1);
+            expect(selecionada).toBe(-1);
+        });
+        it("marca cartas como acertadas", function(){
+            virarCarta(deck, 0);
+            virarCarta(deck, 1);
+            expect(deck[0].acertada).toBe(true);
+            expect(deck[1].acertada).toBe(true);
+        });
+        it("nao pode virar carta já virada", function(){
+            virarCarta(deck, 0);
+            virarCarta(deck, 0);
+            expect(selecionada).toBe(0);
+        });
+        it("nao pode desvirar carta selecionada", function(){
+            virarCarta(deck, 0);
+            virarCarta(deck, 0);
+            expect(deck[0].virado).toBe(true);
+        });
+        it("nao pode dar como acertada carta já selecionada", function(){
+            virarCarta(deck, 0);
+            virarCarta(deck, 0);
+            expect(deck[0].acertada).not.toBe(true);
+        });
+        it("marca cartas como acertadas", function(){
+            virarCarta(deck, 0);
+            virarCarta(deck, 1);
+            expect(deck[0].acertada).toBe(true);
+            expect(deck[1].acertada).toBe(true);
+        });
+        
+        
+    });
